284437d387aac578dd9643c9557a45177bafbbf3
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2024-10-20 01:17:48 +0200

    Improve performance in 242

--- problem_list/_242_valid_anagram.py
+++ problem_list/_242_valid_anagram.py
@@ -1,3 +1,11 @@
+from collections import Counter
+
 def valid_anagram(s: str, t: str) -> bool:
-    val = sorted(s) == sorted(t)
-    return val
+    s_letter_count = Counter(s)
+    s_letter_count.subtract(Counter(t))
+    
+    for value in s_letter_count.values():
+        if value != 0:
+            return False
+    
+    return True
