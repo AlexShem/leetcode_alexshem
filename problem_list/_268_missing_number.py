e75ae731456fea07e7719cb2fc9996d002917bd4
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2024-10-19 23:14:21 +0200

    Add missing number problem solution

--- problem_list/_268_missing_number.py
+++ problem_list/_268_missing_number.py
@@ -2,4 +2,7 @@ from typing import List
 
 
 def missing_number(nums: List[int]) -> int:
-    pass
+    nums_len = len(nums)
+    nums_set = set(nums)
+    full_set = set(range(nums_len + 1))
+    return full_set.difference(nums_set).pop()
