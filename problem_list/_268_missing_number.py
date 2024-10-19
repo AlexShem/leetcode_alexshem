5371f4b0db612b63b5ce61c21990019fff37e339
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2024-10-19 23:31:26 +0200

    Refactor missing_number function to improve performance

--- problem_list/_268_missing_number.py
+++ problem_list/_268_missing_number.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 def missing_number(nums: List[int]) -> int:
-    nums_len = len(nums)
-    nums_set = set(nums)
-    full_set = set(range(nums_len + 1))
-    return full_set.difference(nums_set).pop()
+    n = len(nums)
+    total_sum = n * (n + 1) // 2
+    actual_sum = sum(nums)
+    return total_sum - actual_sum
