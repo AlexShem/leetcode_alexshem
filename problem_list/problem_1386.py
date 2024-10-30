5fd838dd7edcef0fba191826c1fc49b030c8f91f
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2024-10-30 23:41:36 +0100

    Solve 1365

--- problem_list/problem_1386.py
+++ problem_list/problem_1386.py
@@ -2,35 +2,25 @@ from collections import defaultdict
 
 
 def max_number_of_families(n: int, reserved_seats: list[list[int]]) -> int:
-    reserved_seats.sort()
-    reserved_by_row = defaultdict(list)
+    reserved_set = defaultdict(set)
+
     for row, seat in reserved_seats:
-        reserved_by_row[row].append(seat)
+        if seat in {2, 3}:
+            reserved_set[row].add(0)
+        elif seat in {4, 5}:
+            reserved_set[row].add(0)
+            reserved_set[row].add(1)
+        elif seat in {6, 7}:
+            reserved_set[row].add(1)
+            reserved_set[row].add(2)
+        elif seat in {8, 9}:
+            reserved_set[row].add(2)
 
-    result = 0
-    for i in range(1, n + 1):
-        if i not in reserved_by_row:
-            result += 2
-            continue
+    result = 2 * n
+    for blocked in reserved_set.values():
+        if len(blocked) == 3:
+            result -= 2
+        else:
+            result -= 1
 
-        reserved_in_row = reserved_by_row[i]
-
-        # Try allocating two families
-        if (len(reserved_in_row) <= 2
-                and reserved_in_row[0] in [1, 10]
-                and reserved_in_row[-1] in [1, 10]):
-            result += 2
-            continue
-
-        # Try allocating one family
-        possible_locations = [(2, 5), (4, 7), (6, 9)]
-        for location in possible_locations:
-            loc_possible = True
-            for taken in reserved_in_row:
-                if taken in range(location[0], location[1] + 1):
-                    loc_possible = False
-                    break
-            if loc_possible:
-                result += 1
-                break
     return result
