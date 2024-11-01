1f692d2b2c471aed9846d401880eaec7ba444503
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2024-11-01 22:56:16 +0100

    Improve 973
    - Improve code readability
    - Calculate distance^2
    - Use `.heappushpop()`

--- problem_list/problem_973.py
+++ problem_list/problem_973.py
@@ -1,21 +1,13 @@
 import heapq
-import math
-
-
-def distance(point_1: list[int], point_2=None) -> float:
-    if point_2 is None:
-        point_2 = [0, 0]
-    return math.sqrt((point_1[0] - point_2[0]) ** 2 + (point_1[1] - point_2[1]) ** 2)
 
 
 def k_closest(points: list[list[int]], k: int) -> list[list[int]]:
     heap = []
     for point in points:
-        dist = distance(point)
-        if len(heap) < k:
-            heapq.heappush(heap, (-dist, point))
-        elif -dist > heap[0][0]:
-            heapq.heappop(heap)
+        dist = point[0] * point[0] + point[1] * point[1]
+        if len(heap) == k:
+            heapq.heappushpop(heap, (-dist, point))
+        else:
             heapq.heappush(heap, (-dist, point))
 
     return [closest_points for _, closest_points in heap]
