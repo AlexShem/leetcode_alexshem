868681b6ddcc617560d0233554536af15d4a7f83
Aleksandr Shemendyuk <alex.shemendyuk@gmail.com>
2024-11-06 00:01:44 +0100

    Solve 386
    #hard
    - Supports unary minus as '~' sign
    - Tokenize the string

--- problem_list/problem_224.py
+++ problem_list/problem_224.py
@@ -3,27 +3,50 @@ from collections import deque
 operator_precedence = {
     "(": 4,
     ")": 4,
-    "*": 3,
-    "/": 3,
-    "+": 2,
-    "-": 2,
-    # 4: '()',  # Parentheses
-    # 3: '*/',  # Binary multiplication and division
-    # 2: '+-',  # Binary plus and minus
-    # 1: '',  # Reserved
+    "~": 3,
+    "*": 2,
+    "/": 2,
+    "+": 1,
+    "-": 1,
 }
 
 operator_list = '+-*/()'
 
 
+def tokenize_string(s: str) -> list[str]:
+    tokens = []
+    i = 0
+
+    while i < len(s):
+        char = s[i]
+        if char.isdigit():
+            num = char
+            i += 1
+            while i < len(s) and s[i].isdigit():
+                num += s[i]
+                i += 1
+            tokens.append(num)
+            continue
+        elif char == '-':
+            if i == 0 or (i > 0 and s[i - 1] == '('):
+                tokens.append('~')
+            else:
+                tokens.append('-')
+        elif char in '+*/()':
+            tokens.append(char)
+        i += 1
+    return tokens
+
+
 def to_reverse_polish_notation(s: str) -> list[str]:
     output = []
+    s_tokenized = tokenize_string(s)
     operator_stack = deque([])
 
-    for token in s:  # Assumes there a number can only be a digit
+    for token in s_tokenized:
         if token.isnumeric():
             output.append(token)
-        elif token in '+-*/':
+        elif token in '+-*/~':
             while len(operator_stack) and operator_stack[-1] != '(' and (
                     operator_precedence[operator_stack[-1]] >= operator_precedence[token]):
                 output.append(operator_stack.pop())
@@ -56,10 +79,15 @@ def evaluate_from_rpn(rpn_stack: list[str]) -> int:
                 stack.append(operand_1 + operand_2)
             elif token == '-':
                 stack.append(operand_1 - operand_2)
+            elif token == '-':
+                stack.append(operand_1 - operand_2)
             elif token == '*':
                 stack.append(operand_1 * operand_2)
             elif token == '/':
                 stack.append(operand_1 / operand_2)
+        elif token == '~':
+            operand = stack.pop()
+            stack.append(-operand)
     return stack.pop()
 
 
@@ -86,4 +114,12 @@ if __name__ == "__main__":
 
     expression = "-(2+1)"
     result = calculate(expression)  # Expected Output: -3
+    print(result)
+
+    expression = "2147483647"
+    result = calculate(expression)  # Expected Output: 2147483647
+    print(result)
+
+    expression = "1-(     -2)"
+    result = calculate(expression)  # Expected Output: 3
     print(result)
